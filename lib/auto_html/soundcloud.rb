2df0cdba92afa66b5f080e8942f5a2c2571a167f
Pierre Olivier Martel <pomartel@coderubik.com>
2020-08-11 16:49:11 -0400

    fix

--- lib/auto_html/soundcloud.rb
+++ lib/auto_html/soundcloud.rb
@@ -11,9 +11,9 @@ module AutoHtml
     end
 
     def call(text)
-      text.gsub(/(https:\/\/)?(www.)?soundcloud\.com\/\S*/) do |match|
+      text.gsub(/(https?:\/\/)?(www.)?soundcloud\.com\/\S*/) do |match|
         new_uri = match.to_s
-        new_uri = (new_uri =~ /^https\:\/\/.*/) ? new_uri : "https://#{new_uri}"
+        new_uri = (new_uri =~ /^https?\:\/\/.*/) ? new_uri : "https://#{new_uri}"
         tag(:iframe, iframe_attributes(new_uri)) { '' }
       end
     end
--- spec/auto_html/soundcloud_spec.rb
+++ spec/auto_html/soundcloud_spec.rb
@@ -5,4 +5,9 @@ RSpec.describe AutoHtml::Soundcloud do
     result = subject.call('https://soundcloud.com/liluzivert/future-lil-uzi-vert-patek')
     expect(result).to eq '<iframe width="100%" heigth="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https://soundcloud.com/liluzivert/future-lil-uzi-vert-patek&hide_related=true&show_comments=false&show_user=false&show_reposts=false&show_teaser=false&visual=true"></iframe>'
   end
+
+  it 'transform soundcloud url without https' do
+    result = subject.call('http://soundcloud.com/liluzivert/future-lil-uzi-vert-patek')
+    expect(result).to eq '<iframe width="100%" heigth="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=http://soundcloud.com/liluzivert/future-lil-uzi-vert-patek&hide_related=true&show_comments=false&show_user=false&show_reposts=false&show_teaser=false&visual=true"></iframe>'
+  end
 end
